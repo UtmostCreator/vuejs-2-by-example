d96453d4e285124890e99c48595bbaa049e34a1a
MasterMindNET <romazahrypa@gmail.com>
2021-04-13 16:51:11 +0300

    feat: you can use method sayHello, to ouput message; vue supports accessing methods and data property in html files w/o extra things; vue provices easy access to main vue object to access data inside of methods, so you can use 'this.fieldName' to access this prop or 'this.methodName()' to call a method

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ new Vue({
         };
     },
     methods: {
+        sayHello: function() {
+            return 'Hello!';
+            // return {test: 'testString', obj: {newprop: 'propValue'}}; // everything will be converted to a string
+            // return ['hello!', 'asdfasd']; // something that is a string; not object/array
+        },
         addGoal: function() {
-            this.goals.push(this.enteredValue);
+            this.goals.push(this.enteredValue); // vue js creates easy access for us to use 'this' here.
         },
         changeTitle: function(event) {
-            this.title = event.target.value;
+            this.title = event.target.value; // vue js creates easy access for us to use 'this' here.
         }
     }
   });
