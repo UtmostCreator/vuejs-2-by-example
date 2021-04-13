aa798ed16fe066332dab822c8bfd1ae63bc4b198
MasterMindNET <romazahrypa@gmail.com>
2021-04-13 21:06:24 +0300

    feat: keyup and keydown.enter

--- app.js
+++ app.js
@@ -2,24 +2,16 @@ new Vue({
     el: "#app",
     data() {
         return {
-            name: '',
-            age: '',
-            rndNumber: '',
-            imgUrl: 'https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg?v=47faa659a05e',
-            rndTopLimit: 10,
-            coords: {x: 0, y: 0}
+            enterValue: '',
+            keyupValue: ''
         }
     },
     methods: {
-        getUserNameAndAge: function() {
-            return ` Name: ${this.name} (Age: ${this.age})`;
+        keyup: function(event) {
+            this.keyupValue = event.target.value;
         },
-        generateRandomNumber: function() {
-            this.rndNumber = Math.floor(Math.random() * this.rndTopLimit);
-        },
-        trackCoords: function(event) {
-            this.coords.x = event.clientX;
-            this.coords.y = event.clientY;
+        enterPressed: function(event) {
+            this.enterValue = event.target.value;
         }
     }
   });
