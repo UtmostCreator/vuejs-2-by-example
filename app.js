9135e7a4a941f93503ebb4fe73e97cb3c7ab626f
MasterMindNET <romazahrypa@gmail.com>
2021-04-13 19:44:53 +0300

    feat: two-way biding (v-model="var" or :attrName="" as a shorthand) + one-way binding (v-bind="")

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ new Vue({
     el: "#app",
     data() {
         return {
-
+            name: '',
+            age: '',
+            rndNumber: '',
+            imgUrl: 'https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg?v=47faa659a05e',
+            rndTopLimit: 10
         }
     },
     methods: {
-
+        getUserNameAndAge: function() {
+            return ` Name: ${this.name} (Age: ${this.age})`;
+        },
+        generateRandomNumber: function() {
+            this.rndNumber = Math.floor(Math.random() * this.rndTopLimit);
+        }
     }
   });
