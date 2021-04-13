1e2dfa68dd21edd3281b19bffbd104053910f8a2
MasterMindNET <romazahrypa@gmail.com>
2021-04-13 18:01:35 +0300

    chor: prepared empty project

--- app.js
+++ app.js
@@ -2,24 +2,10 @@ new Vue({
     el: "#app",
     data() {
         return {
-            goals: [],
-            title: 'DEFAULT Titles',
-            link: 'https://google.com',
-            enteredValue: '',
-            htmlLinkTag: '<a href="https://google.com"> html Link to google from comp</a>'
-        };
+
+        }
     },
     methods: {
-        sayHello: function() {
-            return 'Hello!';
-            // return {test: 'testString', obj: {newprop: 'propValue'}}; // everything will be converted to a string
-            // return ['hello!', 'asdfasd']; // something that is a string; not object/array
-        },
-        addGoal: function() {
-            this.goals.push(this.enteredValue); // vue js creates easy access for us to use 'this' here.
-        },
-        changeTitle: function(event) {
-            this.title = event.target.value; // vue js creates easy access for us to use 'this' here.
-        }
+
     }
   });
