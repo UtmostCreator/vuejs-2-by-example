a4fa30faa511a268d9d81d4190ca4cb69cf2f4c2
MasterMindNET <romazahrypa@gmail.com>
2021-04-14 17:42:07 +0300

    feat: watch count and reset it to 0 after 2 seconds

--- app.js
+++ app.js
@@ -1,52 +1,23 @@
 new Vue({
     el: '#app',
     data: {
-        infinity: Infinity,
-        value: 3,
-        debt: -6,
-        cash: 9,
-        moneyInBank: 15,
+        counter: 0
     },
 
+    // computed properties always work syncronosly (see the prevous branch e54eca0)
     computed: {
-        computedPropRemainingCashFundsIfPaid: function () {
-            console.log('computedPropRemainingCashFundsIfPaid');
-            return this.debt + this.cash;
-        },
-        computedPropRemainingTotalFunds: function () {
-            console.log('computedPropRemainingTotalFunds');
-            return this.cash + this.moneyInBank + this.debt;
+    },
+    // is used when you need some other code when the propeties updates, and when it is not solved by a computed property, then watch is for you
+    watch: {
+        counter: function(value) {
+            let vm = this;
+
+            // because it is a closure, you have to store you app instance in a separate variable.
+            setTimeout(function() {
+                vm.counter = 0;
+            }, 2000);
         }
     },
     methods: {
-        depositFunds: function (from, to, value, limit = false) {
-            if (limit && (this[to] + value) >= 0) { // if you try to return greater value than you owe
-                this[from] += this[to];
-                this[to] = 0;
-            } else if (this[from] > value && this[from] - value >= 0) { // usual deposit
-                this[to] += value;
-                this[from] -= value;
-            } else { // attempt to depost more than you have
-                this[to] += this[from];
-                this[from] = 0;
-            }
-        },
-        repayADebt: function () {
-            this.value = Math.abs(this.value);
-            if (this.debt < 0) {
-                this.depositFunds('cash', 'debt', this.value, true);
-            }
-            console.log('Attempt to repayADebt', this.value);
-        },
-        lendAmount: function () {
-            this.depositFunds('infinity', 'debt', -Math.abs(this.value));
-            console.log('Attempt to lendAmount', this.value);
-        },
-        withdraw: function () {
-            if (this.moneyInBank) {
-                this.depositFunds('moneyInBank', 'cash', this.value);
-            }
-            console.log('Attempt to withdraw', this.value);
-        }
     }
 });
