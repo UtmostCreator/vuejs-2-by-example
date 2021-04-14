26cebaec9883a031b73a5b727e7d9058b391d091
MasterMindNET <romazahrypa@gmail.com>
2021-04-14 18:35:14 +0300

    feat: adding conditionally styles with :class="{red: propery}" or :class="divClasses" using computed property

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ new Vue({
     el: "#app",
     data() {
         return {
-
+            attachRed: false
+        }
+    },
+    computed: {
+        divClasses: function() {
+            return {
+                red: this.attachRed,
+                blue: !this.attachRed
+            }
         }
     },
     methods: {
