640aa5875f32f3ce9cddcb6f6f25dec8e3f417aa
MasterMindNET <romazahrypa@gmail.com>
2021-04-14 18:52:32 +0300

    chor: prepared clean app

--- app.js
+++ app.js
@@ -2,17 +2,9 @@ new Vue({
     el: "#app",
     data() {
         return {
-            attachRed: false,
-            color: ''
         }
     },
     computed: {
-        divClasses: function() {
-            return {
-                red: this.attachRed,
-                blue: !this.attachRed
-            }
-        }
     },
     methods: {
     }
