109ddabe333d6d80a9b931047194f6d37a953f39
MasterMindNET <romazahrypa@gmail.com>
2021-04-14 19:03:00 +0300

    feat: using array to set styles :style="[myStyle, {backgroundColor: color, color: color, height: width + 'px'}]"

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ new Vue({
     el: "#app",
     data() {
         return {
+            color: '',
+            width: 100
         }
     },
     computed: {
+        myStyle: function() {
+            return {
+                backgroundColor: this.color,
+                width: this.width + 'px'
+            }
+        }
     },
     methods: {
     }
