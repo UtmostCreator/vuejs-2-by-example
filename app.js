685475c552332146468ffac70d2e225ac93b265c
MasterMindNET <romazahrypa@gmail.com>
2021-04-14 17:48:29 +0300

    feat: watch property example; the result watch prop will be calculated only after a computed result will be changed

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ new Vue({
 
     // computed properties always work syncronosly (see the prevous branch e54eca0)
     computed: {
+        result: function() {
+            return this.counter > 5 ? "Greater than 5" : "Less than 5";
+        }
     },
     // is used when you need some other code when the propeties updates, and when it is not solved by a computed property, then watch is for you
     watch: {
-        counter: function(value) {
+        // this wil be changed only after the computed result will be re-rendered re-cached!!!
+        result: function(value) {
             let vm = this;
 
             // because it is a closure, you have to store you app instance in a separate variable.
             setTimeout(function() {
                 vm.counter = 0;
-            }, 2000);
+            }, 5000);
         }
     },
     methods: {
