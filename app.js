0640b856d72aa90ab7de1cea4605646648a3a2ea
MasterMindNET <romazahrypa@gmail.com>
2021-04-14 20:05:39 +0300

    feat: changing classes and styles with interval; you can stop this interval

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ new Vue({
     el: "#app",
     data() {
         return {
-            color: '#ccc',
-            colorArr: ['red', 'green', 'blue', 'steelblue', '#abd']
+            currClass: '',
+            color: '',
+            handle: null,
+            bgArrStyleClasses: ['red', 'green', 'blue', 'steelblue', '#abd'],
+            bgClasses: ['red', 'green', 'steelblue']
         };
     },
     computed: {
-        bgColor: function () {
+        bgStyleColor: function () {
             return {
                 backgroundColor: this.color
             }
@@ -25,10 +28,21 @@ new Vue({
             // console.log(Math.floor(2.99)); // return 2
             // so this works like: we get a value from 0.0 to 0.99... then we 3 -1 + 1 => 3 * 0.99 = 2.97 Math.floor(2.97) = 2; then + 1 = 3 which is our max value.
             console.log(vm.getRandomArbitrary(1, 2));
-            setInterval(function () {
-                vm.color = vm.colorArr[vm.getRandomArbitrary(0, vm.colorArr.length)];
+            this.handle = setInterval(function () {
+                vm.color = vm.bgArrStyleClasses[vm.getRandomArbitrary(0, vm.bgArrStyleClasses.length - 1)];
                 console.log(vm.color)
             }, 2000);
+        },
+        startEffectWithClasses: function () {
+            let vm = this;
+            this.handle = setInterval(function () {
+                vm.currClass = vm.bgClasses[vm.getRandomArbitrary(0, vm.bgClasses.length - 1)];
+                console.log(vm.currClass);
+            }, 2000);
+        },
+        stopInterval: function () {
+            clearInterval(this.handle);
+            this.handle = null;
         }
     }
 });
