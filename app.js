1e1b0551a34b45c505b6a83fccdf0a358192b415
MasterMindNET <romazahrypa@gmail.com>
2021-04-15 19:13:18 +0300

    feat: output a list of items with v-for

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ new Vue({
     el: "#app",
     data() {
         return {
-
+            products: [
+                {name: 1, q: 2},
+                {name: 2, q: 5},
+                {name: 3, q: 3},
+                {name: 4, q: 2},
+            ]
         };
     },
     methods: {
