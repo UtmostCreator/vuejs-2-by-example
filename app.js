16029e3a5bdb4ec004f8eb76a347f6c0f7ea66f2
MasterMindNET <romazahrypa@gmail.com>
2021-04-16 00:30:27 +0300

    feat: using v-if, v-for for outputting arrays, multidimentional objects and so on using ifs to check it; using v-show to display none...

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ new Vue({
     el: "#app",
     data() {
         return {
-
+            show: true,
+            books: [
+                {
+                    title: "Title Unbeatable",
+                    published: 2020,
+                    author: "David Amaron",
+                },
+                {
+                    title: "Title New",
+                    published: 2004,
+                    author: "Brad Avilston",
+                },
+                {
+                    title: "Pet treatments",
+                    published: 2021,
+                    author: "Britney Drey",
+                },
+            ],
+            experiment: [
+                {
+                    name: 'testeojb',
+                    data: [1, 1.23, 5.33, 9.44, 99999,999900098]
+                }
+            ]
         };
     },
     methods: {
     }
-  });
+});
