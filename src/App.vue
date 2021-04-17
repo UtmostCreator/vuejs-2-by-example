eeec776d93b6c197df78cda2e9177d804c91bb1e
MasterMindNET <romazahrypa@gmail.com>
2021-04-17 18:39:29 +0300

    feat: added new Home component which we imported in main.js and then use it in a template with a specified tag; Component template should contain exactly one root element. If you are using v-if on multiple elements, use v-else-if to chain them instead

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <p>Server status: {{status}}</p>
+    <div>
+        Empty
+    </div>
 </template>
 
 <script>
 export default {
-  data: function() {
-      return {
-          status: 'Critical',
-      }
-  }
 }
 </script>
 
