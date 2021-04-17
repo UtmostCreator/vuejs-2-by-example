75586395a2d4716061703f0412b6df9f75ffc445
MasterMindNET <romazahrypa@gmail.com>
2021-04-17 16:03:10 +0300

    feat: using lifecycle hooks with simple examples

--- app.js
+++ app.js
@@ -3,6 +3,36 @@ new Vue({
     data: {
         title: 'Title'
     },
+    beforeCreate: function() {
+        console.log('beforeCreate');
+    },
+    created: function() {
+        console.log('created');
+    },
+    beforeMount: function() {
+        console.log('beforeMount');
+    },
+    mounted: function() {
+        console.log('mounted');
+    },
+    beforeUpdate: function() {
+        console.log('beforeUpdate');
+    },
+    updated: function() {
+        console.log('updated');
+    },
+    beforeDestroy: function() {
+        console.log('beforeDestroy');
+    },
+    destroyed: function() {
+        console.log('destroyed');
+    },
     methods: {
+        updateTitle: function () {
+            this.title += 'updated';
+        },
+        destroy: function() {
+            this.$destroy();
+        }
     }
   });
