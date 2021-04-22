4a109a54c82f6cdb30db8eec84d86c65b0504b21
MasterMindNET <romazahrypa@gmail.com>
2021-04-23 02:04:50 +0300

    chor: unused files deleted

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,6 @@
 
 <script>
 import Header from './components/shared/Header';
-import Servers from './components/server/Servers';
-import ServerDetails from './components/server/ServerDetails';
 import Footer from './components/shared/Footer';
 
 export default {
