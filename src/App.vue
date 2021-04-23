a21a8c50a50a44490242f20f8a44c5e1e2f12487
MasterMindNET <romazahrypa@gmail.com>
2021-04-23 14:49:53 +0300

    chor: removed unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
     <div>
         <app-header></app-header>
         <hr>
-        <div class="servers" style="display: grid; grid: 1fr / 1fr 1fr">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
-        </div>
+
         <hr>
         <app-footer></app-footer>
     </div>
@@ -13,15 +10,11 @@
 
 <script>
 import Header from './components/shared/Header';
-import Servers from './components/server/Servers';
-import ServerDetails from './components/server/ServerDetails';
 import Footer from './components/shared/Footer';
 
 export default {
     components: {
         'app-header': Header,
-        'app-servers': Servers,
-        'app-server-details': ServerDetails,
         'app-footer': Footer
     }
 }
