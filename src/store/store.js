b1c9db07d5aae3ce3bc9d227450f73026325040d
MasterMindNET <romazahrypa@gmail.com>
2021-04-27 19:10:39 +0300

    feat: using getters to remove code duplicates

--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,10 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: { // state name is not arbitrary
         counter: 0,
+    },
+    getters: {
+        doubleCounter: state => {
+            return state.counter * 2;
+        }
     }
 });
