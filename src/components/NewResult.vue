7aa2eb299f5da5760074bd375b63c67446c34836
MasterMindNET <romazahrypa@gmail.com>
2021-04-27 19:39:16 +0300

    feat: adding ourOwnGetterWithinAComponent + babel-preset-stage-3 is required for spread operator (ES6)

--- src/components/NewResult.vue
+++ src/components/NewResult.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <p>Counter result is {{doubleCounter}}</p>
-        <p>{{stringCounter}}</p>
+        <p>Counter result is {{ doubleCounter }}</p>
+        <p>{{ stringCounter }}</p>
+        <p>{{ ourOwnGetterWithinAComponent }}</p>
     </div>
 </template>
 
@@ -9,10 +10,21 @@
 import {mapGetters} from 'vuex';
 
 export default {
-    computed: mapGetters([
-        'doubleCounter',
-        'stringCounter',
-    ])}
+    // we NEED to add in package.json (babel-preset-stage-3) or run 'npm i -D babel-preset-stage-3' it adds a support for spread operator
+    // + .babelrc presets.
+    computed: {
+        // mapGetters is an object which holds all the mapped methods (computed properties);
+        // spread operator pull them all out of there, and create separate key values pairs for each of them
+        // this way it allows us to add our custom getters to already existing methods which are get created automatically into this object (computed)
+        ...mapGetters([
+            'doubleCounter',
+            'stringCounter',
+        ]),
+        ourOwnGetterWithinAComponent() {
+            return 'ourOwnGetterWithinAComponent';
+        }
+    }
+}
 </script>
 
 <style scoped></style>
